20b2ba3d6b95efd178f5db3b84f8ba2c73c20493
KeerthanaNKK <keerthanank96@gmail.com>
2021-07-10 18:37:17 +0530

    Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'ZZ Kurti Full Length',
+            category:'Kurti',
+            image: '/images/1.jpg',
+            price:120,
+            brand:'ZZ',
+            rating:4.5,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Zollo Kurti Full Length',
+            category:'Top',
+            image: '/images/1.jpg',
+            price:550,
+            brand:'Zollo',
+            rating:4,
+            numReviews:12,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Maxx Umbrella Top',
+            category:'Kurti',
+            image: '/images/2.jpg',
+            price:1000,
+            brand:'Maxx',
+            rating:4.7,
+            numReviews:18,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Anna Full Length Printed Kurti',
+            category:'Kurti',
+            image: '/images/3.jpg',
+            price:200,
+            brand:'Anna',
+            rating:4.2,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Bella Kurti',
+            category:'Kurti',
+            image: '/images/2.jpg',
+            price:350,
+            brand:'Bella',
+            rating:3,
+            numReviews:2,
+            description: 'high quality product'
+        },
+    ],
+};
+
+export default data;
